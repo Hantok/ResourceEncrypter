3c769f4c3f0f10a6af17051599e1dffa89e9e1d3
Roman Slysh <monar.ua@gmail.com>
2016-01-12 18:46:42 +0200

    Initial Commit

---
+++ ResourceEngrypter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ResourceEngrypter
+//
+//  Created by Roman Slysh on 1/12/16.
+//  Copyright © 2016 Roman Slysh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
